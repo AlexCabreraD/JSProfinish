9cd64a81e7aaa04f3afe896c7544634797eb3bb9
AlexCabreraD <109772528+AlexCabreraD@users.noreply.github.com>
2024-06-03 17:40:53 -0600

    Updates

--- src/app/theme/myThemeProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import * as React from "react";
-import { ThemeProvider } from "@mui/material";
-import { useEffect } from "react";
-
-export function MyThemeProvider({ children, ...props }: any) {
-    return <ThemeProvider {...props}>{children}</ThemeProvider>;
-}

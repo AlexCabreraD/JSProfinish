9a5b5bb77fb53cf508b504cb131c61bfde321a8e
AlexCabreraD <109772528+AlexCabreraD@users.noreply.github.com>
2024-06-05 08:47:12 -0600

    fix building error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <Benefits />
             <HowWorks />
             <Cta />
-            <Testimonials padded={false} />
+            <Testimonials />
             <Contact />
         </Box>
     );

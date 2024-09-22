74f15d9390f9e4b46ee26052230116d10c52f065
AlexCabreraD <109772528+AlexCabreraD@users.noreply.github.com>
2024-09-22 12:04:24 -0600

    force redirect to https

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/(.*)',
+                has: [{ type: 'host', value: 'jsprofinish-utah.com' }],
+                destination: 'https://jsprofinish-utah.com/:path*',
+                permanent: true,  // This sets a 301 permanent redirect
+            },
+        ];
+    },
+};
 
 export default nextConfig;
